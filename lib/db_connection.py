8e4ef30a67c755c1f8b1e9388bc38573ec30426a
Mahima_Akula <145792829+limitless-soul@users.noreply.github.com>
2023-11-07 19:36:30 -0500

    db_connection

---
+++ lib/db_connection.py
@@ -0,0 +1,33 @@
+import mysql.connector
+db_config = {
+    'host': 'sql10.freemysqlhosting.net',
+    'user': 'sql10659817',
+    'password': 'Lkrtg7NWLx',
+    'database': 'sql10659817'
+}
+
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+try:
+    cursor.execute("SELECT 1")
+    result = cursor.fetchone()
+    if result:
+        print("Connection is working.")
+    else:
+        print("Connection might have issues.")
+
+except mysql.connector.Error as e:
+    print(f"Error executing the query: {e}")
+
+cursor.close()
+
+metadata = {
+    'filename': 'example.txt',
+    'timestamp': '2023-11-07 18:34:00',
+    'user_id': 123
+}
+
+insert_query = "INSERT INTO metadata (filename, timestamp, user_id) VALUES (%s, %s, %s)"
+values = (metadata['filename'], metadata['timestamp'], metadata['user_id'])
+print(values)
