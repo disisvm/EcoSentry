fc3a53054ea994f31ebb3446e9fe49297ffe7e59
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-11-07 20:03:47 -0500

    lib

--- lib/db_connection.py
+++ lib/db_connection.py
@@ -1,4 +1,5 @@
 import mysql.connector
+
 db_config = {
     'host': 'sql10.freemysqlhosting.net',
     'user': 'sql10659817',
@@ -20,18 +21,17 @@ try:
 except mysql.connector.Error as e:
     print(f"Error executing the query: {e}")
 
-
 metadata = {
     'filename': 'example.txt',
     'timestamp': '2023-11-07 18:34:00',
     'user_id': 123
 }
-#create_query = "create table metadata(filename varchar(100), timestamp datetime, user_id varchar(25))"
-#cursor.execute(create_query)
+# create_query = "create table metadata(filename varchar(100), timestamp datetime, user_id varchar(25))"
+# cursor.execute(create_query)
 
 insert_query = "INSERT INTO metadata (filename, timestamp, user_id) VALUES (%s, %s, %s)"
 values = (metadata['filename'], metadata['timestamp'], metadata['user_id'])
 cursor.execute(insert_query, values)
-#print(values)
+# print(values)
 conn.commit()
 conn.close()
