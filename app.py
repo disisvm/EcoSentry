e172913991cbee704d9fb460b150ecd1acb80570
Jyothi-veeranki <62088988+Jyothi-veeranki@users.noreply.github.com>
2023-11-07 19:16:02 -0500

    dashboard

---
+++ app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+import os
+iface = gr.Interface(
+    fn=identify_species,
+    inputs="directory",
+    outputs="file",
+    title="Wildlife Species Identification",
+    description="Upload a directory containing wildlife images for species identification.",
+    allow_flagging=False  
+)
+
+iface.launch()
