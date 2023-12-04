a96aef838eb563f59f1f447187db54daefcd732a
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-12-04 12:20:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,94 @@
+import streamlit as st
+import os
+from PIL import Image
+import zipfile
+from io import BytesIO
+
+
+# Function to process images in the selected directory
+def process_images(directory_path):
+    # Your image processing logic here
+    # Replace the following example code with your actual processing logic
+    result = []
+    for filename in os.listdir(directory_path):
+        img_path = os.path.join(directory_path, filename)
+        # Replace this with your actual function that processes an image
+        processed_result = process_single_image(img_path)
+        result.append(processed_result)
+    return result
+
+
+# Function to process a single image (replace with your actual logic)
+def process_single_image(image_path):
+    # Your image processing logic here
+    # Replace the following example code with your actual processing logic
+    return {
+        'filename': os.path.basename(image_path),
+        'class': 'Animal',  # Replace with the actual class identified
+        'confidence': 0.85  # Replace with the actual confidence score
+    }
+
+
+# Function to generate and download a zip file
+def download_zip(result):
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+        for item in result:
+            filename = item['filename']
+            class_identified = item['class']
+            confidence = item['confidence']
+            content = f"{filename}\nClass: {class_identified}\nConfidence: {confidence}\n"
+            zip_file.writestr(filename.replace('.jpg', '_result.txt'), content)
+    zip_buffer.seek(0)
+    return zip_buffer
+
+
+# Streamlit app
+def main():
+    st.title("Animal Image Processing App")
+
+    # Sidebar - Select directory with images
+    st.sidebar.header("Select Image Directory")
+    image_directory = st.sidebar.selectbox("Choose a directory:", os.listdir('/path/to/root/folder/'))
+
+    # Process images on button click
+    if st.sidebar.button("Process Images"):
+        result = process_images(image_directory)
+        st.sidebar.success("Image processing completed!")
+
+        # Download results as a zip file
+        zip_buffer = download_zip(result)
+        st.sidebar.markdown(
+            f"### [Download Results as Zip File](data:application/zip;base64,{zip_buffer.read().encode('base64')})"
+        )
+
+        # Display results
+        st.header("Image Processing Results")
+        for item in result:
+            st.write(f"Filename: {item['filename']}")
+            st.write(f"Class Identified: {item['class']}")
+            st.write(f"Confidence: {item['confidence']}")
+            st.write("---")
+
+    # Display graphs (replace with your actual graph generation code)
+    st.header("Graphs")
+    # Your graph generation code here
+
+    # Report section with filters and download button
+    st.header("Reports")
+    start_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
+
+    # Filtered reports (replace with your actual report generation code)
+    filtered_reports = []  # Replace with your actual filtered report data
+
+    # Download button for reports
+    if st.button("Download Reports"):
+        # Your report download logic here
+        st.success("Reports downloaded successfully!")
+
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
 
