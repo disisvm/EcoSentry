45d78817776b830310b3530a6629c33c2cb00697
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-12-05 18:25:17 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from PIL import Image
 import zipfile
 from io import BytesIO
 
+from lib.prediction_pipeline import ensemble_predict
+
 # Function to process images in the selected directory
 def process_images(file_list):
     # Your image processing logic here
@@ -13,19 +15,9 @@ def process_images(file_list):
     for uploaded_file in file_list:
         img_path = BytesIO(uploaded_file.read())
         # Replace this with your actual function that processes an image
-        processed_result = process_single_image(img_path)
+        processed_result = ensemble_predict(img_path)
         result.append(processed_result)
     return result
-
-# Function to process a single image (replace with your actual logic)
-def process_single_image(image_path):
-    # Your image processing logic here
-    # Replace the following example code with your actual processing logic
-    return {
-        'filename': "example.jpg",  # Replace with the actual filename
-        'class': 'Animal',  # Replace with the actual class identified
-        'confidence': 0.85   # Replace with the actual confidence score
-    }
 
 # Function to generate and download a zip file
 def download_zip(result):
@@ -66,23 +58,6 @@ def main():
                 st.write(f"Class Identified: {item['class']}")
                 st.write(f"Confidence: {item['confidence']}")
                 st.write("---")
-
-    # Display graphs (replace with your actual graph generation code)
-    st.header("Graphs")
-    # Your graph generation code here
-
-    # Report section with filters and download button
-    st.sidebar.header("Reports")
-    start_date = st.sidebar.date_input("Start Date")
-    end_date = st.sidebar.date_input("End Date")
-
-    # Filtered reports (replace with your actual report generation code)
-    filtered_reports = []  # Replace with your actual filtered report data
-
-    # Download button for reports
-    if st.sidebar.button("Download Reports"):
-        # Your report download logic here
-        st.sidebar.success("Reports downloaded successfully!")
 
 # Run the Streamlit app
 if __name__ == "__main__":
