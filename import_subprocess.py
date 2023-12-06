2d525439e5207a6361d65fb61ea2f9f6bfec53de
JOBIN PHILIP <112263386+BLK-HWK4713@users.noreply.github.com>
2023-12-05 22:57:51 -0500

    Update import_subprocess.py

--- import_subprocess.py
+++ import_subprocess.py
@@ -5,7 +5,7 @@ with open('changed_files.txt', 'r') as file:
     changed_files = file.read().splitlines()
 
 # Define the maximum number of files per commit
-files_per_commit = 10  # Adjust this value based on your requirements
+files_per_commit = 2  # Adjust this value based on your requirements
 
 # Split the list of changed files into smaller chunks
 file_chunks = [changed_files[i:i + files_per_commit] for i in range(0, len(changed_files), files_per_commit)]
